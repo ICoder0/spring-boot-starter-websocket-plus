246cbe93045d4b1ca04b98092bd946a48dac0774
bofa1ex <bofa1exx@gmail.com>
2020-08-18 20:54:24 +0800

    - fix: 修复失效的*BeanProcessor

--- src/main/java/com/icoder0/websocket/core/package-info.java
+++ src/main/java/com/icoder0/websocket/core/package-info.java
@@ -3,6 +3,6 @@
  * @since 2020/8/12
  * @see com.icoder0.websocket.core.annotation.EnableWebsocketPlus
  * @see com.icoder0.websocket.core.annotation.WebsocketPlusAutoConfiguration
- * @see com.icoder0.websocket.core.annotation.WebsocketAnnotationBeanPostProcessor
+ * @see com.icoder0.websocket.core.annotation.WebsocketPlusBeanPostProcessor
  */
 package com.icoder0.websocket.core;
