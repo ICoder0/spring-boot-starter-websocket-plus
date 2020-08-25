485b0058deb74a160f9a4ca9b4851f2d5a45ef0d
bofa1ex <bofa1exx@gmail.com>
2020-08-25 16:16:51 +0800

    - add: 新增notNull方法

--- spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/utils/Assert.java
+++ spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/utils/Assert.java
@@ -3,6 +3,7 @@ package com.icoder0.websocket.core.utils;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 
@@ -13,6 +14,12 @@ import java.util.function.Supplier;
 @Slf4j
 @UtilityClass
 public class Assert {
+
+    public void notNull(Object data, String message){
+        if (Objects.isNull(data)){
+            throw new IllegalArgumentException(message);
+        }
+    }
 
     public <X extends Throwable> void checkCondition(Boolean condition, Supplier<? extends X> supplier) throws X {
         if (!condition) {
