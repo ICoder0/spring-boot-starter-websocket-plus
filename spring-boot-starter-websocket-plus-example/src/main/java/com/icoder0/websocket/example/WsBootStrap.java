59651cfb03c1c511f601fc769cdf94d428954942
bofa1ex <bofa1exx@gmail.com>
2020-09-03 01:26:21 +0800

    - chore: 持续更新示例.

--- spring-boot-starter-websocket-plus-example/src/main/java/com/icoder0/websocket/example/WsBootStrap.java
+++ spring-boot-starter-websocket-plus-example/src/main/java/com/icoder0/websocket/example/WsBootStrap.java
@@ -1,6 +1,5 @@
 package com.icoder0.websocket.example;
 
-import com.alibaba.fastjson.JSON;
 import com.google.common.collect.ImmutableMap;
 import com.icoder0.websocket.annotation.WebsocketMapping;
 import com.icoder0.websocket.annotation.WebsocketMethodMapping;
@@ -11,12 +10,10 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.socket.BinaryMessage;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.io.IOException;
 
 
 /**
@@ -24,11 +21,11 @@ import java.io.IOException;
  * @since 2020/7/31
  */
 @Slf4j
-@WebsocketMapping("/api/mirai")
+@WebsocketMapping(value = "/api/mirai", prototype = true)
 public class WsBootStrap {
 
     @WebsocketMethodMapping("#inbound.code == 1001")
-    public WsOutboundBean login(WebSocketSession webSocketSession, @Validated WsLoginVO req) {
+    public WsOutboundBean<?> login(WebSocketSession webSocketSession, @Validated WsLoginVO req) {
         log.info("login {}", req);
         webSocketSession.getAttributes().put("account", req.getAccount());
         return WsOutboundBean.ok()
