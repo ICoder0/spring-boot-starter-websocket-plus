689c3fa47b5b7efb31aebbcb86ca1f15e3d1c63e
bofa1ex <bofa1exx@gmail.com>
2020-09-03 21:51:04 +0800

    - update: code类型

--- spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsBusiCode.java
+++ spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsBusiCode.java
@@ -8,23 +8,23 @@ import lombok.Getter;
  */
 @Getter
 public enum WsBusiCode {
-    OK("200", "[%s]", ""),
+    OK(200, "[%s]", ""),
 
-    ILLEGAL_REQUEST_ERROR("400", "检查请求资源规范 [%s]", "check your request message specification [%s]"),
+    ILLEGAL_REQUEST_ERROR(400, "检查请求资源规范 [%s]", "check your request message specification [%s]"),
 
-    AUTH_ERROR("403", "检查认证信息 [%s]", "check your auth arguments [%s]"),
+    AUTH_ERROR(403, "检查认证规范 [%s]", "check your auth arguments [%s]"),
 
-    INTERNAL_ERROR("500", "系统内部异常 [%s]", "system internal exception [%s]"),
+    INTERNAL_ERROR(500, "系统内部异常 [%s]", "system internal exception [%s]"),
 
-    BUSINESS_ERROR("600", "业务数据异常 [%s]", "business data exception [%s]");
+    BUSINESS_ERROR(600, "业务数据异常 [%s]", "business data exception [%s]");
 
-    WsBusiCode(String code, String zhMsg, String enMsg) {
+    WsBusiCode(Integer code, String zhMsg, String enMsg) {
         this.code = code;
         this.zhMsg = zhMsg;
         this.enMsg = enMsg;
     }
 
-    private final String code;
+    private final Integer code;
     private final String zhMsg;
     private final String enMsg;
 }
