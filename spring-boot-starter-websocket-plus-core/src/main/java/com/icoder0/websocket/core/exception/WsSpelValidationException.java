d2d4cd04ce0b0e3ccc7514f9926a791ca334da8e
bofa1ex <bofa1exx@gmail.com>
2020-09-07 02:00:29 +0800

    - chore: 移除spel校验异常类的冗余构造方法.

--- spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsSpelValidationException.java
+++ spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsSpelValidationException.java
@@ -1,5 +1,7 @@
 package com.icoder0.websocket.core.exception;
 
+import com.icoder0.websocket.core.model.WsBusiCode;
+
 /**
  * @author bofa1ex
  * @since 2020/8/12
@@ -8,13 +10,5 @@ public class WsSpelValidationException extends WsException {
 
     public WsSpelValidationException() {
         super(WsBusiCode.ILLEGAL_REQUEST_ERROR);
-    }
-
-    public WsSpelValidationException(String message) {
-        super(WsBusiCode.ILLEGAL_REQUEST_ERROR, message);
-    }
-
-    public WsSpelValidationException(String message, Throwable cause) {
-        super(WsBusiCode.ILLEGAL_REQUEST_ERROR, message, cause);
     }
 }
