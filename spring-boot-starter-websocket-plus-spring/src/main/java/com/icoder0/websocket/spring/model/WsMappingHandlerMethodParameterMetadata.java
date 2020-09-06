fe757ac46cd3403a7bd7752fd8420a42b8f85903
bofa1ex <bofa1exx@gmail.com>
2020-09-07 02:05:36 +0800

    - add: 新增对@WebsocketPayload和@WebsocketHeader注解的提取逻辑.

--- spring-boot-starter-websocket-plus-spring/src/main/java/com/icoder0/websocket/spring/model/WsMappingHandlerMethodParameterMetadata.java
+++ spring-boot-starter-websocket-plus-spring/src/main/java/com/icoder0/websocket/spring/model/WsMappingHandlerMethodParameterMetadata.java
@@ -4,11 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.util.TypeUtils;
-import com.icoder0.websocket.core.exception.WsBusiCode;
 import com.icoder0.websocket.core.exception.WsException;
-import com.icoder0.websocket.core.exception.WsExceptionTemplate;
-import com.icoder0.websocket.core.exception.WsRequestParamException;
-import com.icoder0.websocket.core.utils.Assert;
+import com.icoder0.websocket.core.model.WsBusiCode;
 import io.netty.buffer.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,8 +14,6 @@ import org.springframework.web.socket.*;
 
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * @author bofa1ex
@@ -27,13 +22,12 @@ import java.util.Optional;
 @Builder
 public class WsMappingHandlerMethodParameterMetadata {
 
-    private final Class<?> payloadDecodeClazz;
+    private final Class<?> inboundBeanClazz;
+
     private final String payloadParamsDecodeName;
 
-    private final String payloadSpecification;
-    private final String payloadParamSpecification;
-
     private final Method method;
+    @Getter
     private final String name;
     private final String defaultValue;
     private final boolean require;
@@ -42,54 +36,34 @@ public class WsMappingHandlerMethodParameterMetadata {
     @Getter
     private final boolean validated;
 
+    /* method-parameter state */
+    private final boolean isHeader;
+    private final boolean isNormal;
+
+    /**
+     * 只考虑抽取的逻辑.
+     * 上行数据格式规范的校验和异常处理在aop#handleInboundMessage入口解决.
+     */
     public Object extractArg(WebSocketSession session, WebSocketMessage<?> webSocketMessage) {
         if (org.springframework.util.TypeUtils.isAssignable(WebSocketSession.class, type)) {
             return session;
         }
-        // Text message
         if (org.springframework.util.TypeUtils.isAssignable(TextMessage.class, webSocketMessage.getClass())) {
-            final TextMessage textMessage = TypeUtils.cast(webSocketMessage, TextMessage.class, ParserConfig.getGlobalInstance());
             if (org.springframework.util.TypeUtils.isAssignable(TextMessage.class, type)) {
                 return webSocketMessage;
             }
-            // primitive type
-            if (ClassUtils.isPrimitiveOrWrapper(type) || org.springframework.util.TypeUtils.isAssignable(CharSequence.class, type)) {
-                final JSONObject payload = Optional.ofNullable(JSON.parseObject(textMessage.getPayload()))
-                        .orElseThrow(() -> new WsRequestParamException(String.format(
-                                WsExceptionTemplate.REQUEST_PARAMETER_PAYLOAD_SPECIFICATION_ERROR, payloadSpecification
-                        )));
-                // sequence, version, code.
-                if (payload.containsKey(name)) {
-                    return payload.getObject(name, type);
-                }
-
-                final JSONObject payloadParams = Optional.ofNullable(payload.getJSONObject(payloadParamsDecodeName))
-                        .orElseThrow(() -> new WsRequestParamException(String.format(
-                                WsExceptionTemplate.REQUEST_PARAMETER_PAYLOAD_PARAMS_SPECIFICATION_ERROR, payloadParamSpecification
-                        )));
-                // payload params require to contain parameter ?
-                Assert.checkXorCondition(!payloadParams.containsKey(name) && require,
-                        () -> new WsRequestParamException(String.format(
-                                WsExceptionTemplate.REQUEST_PARAMETER_NONE_MATCH, method.getName(), name)
-                        )
-                );
-                return payloadParams.getObject(name, type);
-            }
-            if (org.springframework.util.TypeUtils.isAssignable(Map.class, type)) {
-                return JSON.parseObject(textMessage.getPayload(), Map.class);
-            }
-            if (org.springframework.util.TypeUtils.isAssignable(payloadDecodeClazz, type)) {
-                return JSON.parseObject(textMessage.getPayload(), payloadDecodeClazz);
-            }
-            final JSONObject payload = Optional.ofNullable(JSON.parseObject(textMessage.getPayload()))
-                    .orElseThrow(() -> new WsRequestParamException(String.format(
-                            WsExceptionTemplate.REQUEST_PARAMETER_PAYLOAD_SPECIFICATION_ERROR, payloadSpecification
-                    )));
-            return payload.getObject(payloadParamsDecodeName, type);
+            final TextMessage textMessage = TypeUtils.cast(webSocketMessage, TextMessage.class, ParserConfig.getGlobalInstance());
+            final JSONObject payload = JSON.parseObject(textMessage.getPayload());
+            final JSONObject payloadParams = payload.getJSONObject(payloadParamsDecodeName);
+            return isNormal ?
+                    ClassUtils.isPrimitiveOrWrapper(type) || org.springframework.util.TypeUtils.isAssignable(CharSequence.class, type) ?
+                    // 如果没有@WebsocketPayload和@WebsocketHeader修饰
+                    // 如果是基本类型或者CharSequence类型, 则从payload#params中提取, 反之Map,Pojo类型从payload当前层级提取.
+                    payloadParams.getObject(name, type) : payload.getObject(payloadParamsDecodeName, type) : isHeader ?
+                    // 如果是@WebsocketHeader, 从payload当前层级中提取, 反之从payload#params中提取.
+                    payload.getObject(name, type) : payloadParams.getObject(name, type);
         }
 
-
-        // Binary message
         if (org.springframework.util.TypeUtils.isAssignable(BinaryMessage.class, webSocketMessage.getClass())) {
             final BinaryMessage binaryMessage = TypeUtils.cast(webSocketMessage, BinaryMessage.class, ParserConfig.getGlobalInstance());
 
@@ -104,7 +78,6 @@ public class WsMappingHandlerMethodParameterMetadata {
             }
             return binaryMessage;
         }
-
 
         if (org.springframework.util.TypeUtils.isAssignable(PingMessage.class, type)) {
             return webSocketMessage;
