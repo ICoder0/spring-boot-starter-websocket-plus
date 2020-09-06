dd7d55000134f9c64d9a115a87f8d27acdb22614
bofa1ex <bofa1exx@gmail.com>
2020-09-07 01:56:22 +0800

    - update: emit方法对data入参的类型限制, 兼容旧版本的emitIgnore方法. - add: 新增自动注入上行数据的消息序号逻辑.

--- spring-boot-starter-websocket-plus-spring/src/main/java/com/icoder0/websocket/spring/utils/WebsocketMessageEmitter.java
+++ spring-boot-starter-websocket-plus-spring/src/main/java/com/icoder0/websocket/spring/utils/WebsocketMessageEmitter.java
@@ -1,12 +1,15 @@
 package com.icoder0.websocket.spring.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.icoder0.websocket.core.model.WsOutboundBeanSpecification;
+import com.icoder0.websocket.spring.WebsocketPlusProperties;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * @author bofa1ex
@@ -16,13 +19,31 @@ import java.io.IOException;
 @UtilityClass
 public class WebsocketMessageEmitter {
 
-    public void emit(Object data, WebSocketSession session) {
+    public <T extends WsOutboundBeanSpecification> void emit(T data, WebSocketSession session) {
+        _autoInjectSequence(data,session);
         final String json = JSON.toJSONString(data);
-        log.info("{} OUTBOUND {}", session.getRemoteAddress() + "@" + session.getId().substring(0, 4), json);
+        log.info("{} OUTBOUND {}", session.getRemoteAddress(), json);
         try {
             session.sendMessage(new TextMessage(json));
         } catch (IOException e) {
             log.error("{} send message {} error", session, data);
         }
     }
+
+    @SuppressWarnings("不建议使用, 尽可能走#emit#检查下行数据格式约束")
+    public void emitIgnore(Object data, WebSocketSession session) {
+        final String json = JSON.toJSONString(data);
+        log.info("{} OUTBOUND {}", session.getRemoteAddress(), json);
+        try {
+            session.sendMessage(new TextMessage(json));
+        } catch (IOException e) {
+            log.error("{} send message {} error", session, data);
+        }
+    }
+
+    <T extends WsOutboundBeanSpecification> void _autoInjectSequence(T data, WebSocketSession session){
+        /* @see DefaultWebsocketMessageCustomizer#customize, 默认在原生缓存attributes中注入了上行数据得到的函数枚举和消息序号. */
+        data.setSequence(Optional.ofNullable(data.sequence()).orElse(session.getAttributes().get(WebsocketPlusProperties.payloadSequenceDecodeName)));
+    }
+
 }
