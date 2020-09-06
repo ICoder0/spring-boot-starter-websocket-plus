c921975e2759885e15c075f0edf882b90a0534fd
bofa1ex <bofa1exx@gmail.com>
2020-09-07 02:03:51 +0800

    - chore: 更新详细注释. - add: 新增默认WebsocketMessageCustomizer实现, 上行数据header字段的缓存处理.

--- spring-boot-starter-websocket-plus-autoconfigure/src/main/java/com/icoder0/websocket/spring/configuration/WebsocketPlusAutoConfiguration.java
+++ spring-boot-starter-websocket-plus-autoconfigure/src/main/java/com/icoder0/websocket/spring/configuration/WebsocketPlusAutoConfiguration.java
@@ -1,12 +1,8 @@
 package com.icoder0.websocket.spring.configuration;
 
-
-import com.icoder0.websocket.annotation.WebsocketMapping;
 import com.icoder0.websocket.spring.WebsocketPlusProperties;
 import com.icoder0.websocket.spring.annotation.EnableWebsocketPlus;
-import com.icoder0.websocket.spring.aop.DefaultWebsocketMessageAspectHandler;
-import com.icoder0.websocket.spring.aop.WebsocketMessageArchetypeAspect;
-import com.icoder0.websocket.spring.aop.WebsocketMessageAspectHandler;
+import com.icoder0.websocket.spring.aop.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -15,11 +11,16 @@ import org.springframework.context.annotation.*;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
- * {@code @Configuration} class that registers a bean capable of processing Spring's @{@link WebsocketMapping etc} annotation.
+ * {@code @Configuration} class that registers a bean capable of processing Spring's annotation like above.
+ * {@code WebsocketMapping}
+ * {@code WebsocketMethodMapping}
  *
  * @author bofa1ex
  * @see EnableWebsocketPlus
- * @see org.springframework.context.annotation.EnableAspectJAutoProxy
+ * @see WebsocketMessageCustomizer 用于自定义行为, 处理上行数据.
+ * @see DefaultWebsocketMessageCustomizer 默认实现缓存上行数据的seq, functionCode, params.
+ * @see WebsocketMessageAspectHandler 用于自定义代理行为, 处理上行数据, 下行数据, 建立连接, 断开连接, 传输出错.
+ * @see DefaultWebsocketMessageAspectHandler 默认实现各行为的日志打印, 上行数据规范的校验, 下行数据的自动下发, 建议用户复写建立连接和断开连接的缓存行为.
  * @since 2020/8/1
  */
 @Configuration
@@ -29,6 +30,9 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class WebsocketPlusAutoConfiguration {
 
+    /**
+     * 用户可自定义实现WebsocketMessageAspectHandler覆盖默认实现.
+     */
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     @ConditionalOnMissingBean(WebsocketMessageAspectHandler.class)
@@ -37,8 +41,27 @@ public class WebsocketPlusAutoConfiguration {
     }
 
     /**
-     * @code @Aspect 注解修饰的bean对象必须在WebsocketPlusBeanFactoryPostProcessor前注入到容器, 否则在#createBean时, 无法对注入的bean对象
-     * 实现织入cglib代理.
+     * 用户可自定义实现WebsocketMessageCustomizer, 加入更多对上行数据处理的行为.
+     * 但是默认实现不可覆盖.
+     */
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public WebsocketMessageCustomizer websocketMessageCustomizer() {
+        return new DefaultWebsocketMessageCustomizer();
+    }
+
+    /**
+     * WebsocketMessageCustomizer的注册表, 提供对上行数据处理行为的批处理.
+     */
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public WebsocketMessageCustomizerRegistry websocketMessageCustomizerRegistry(){
+        return new WebsocketMessageCustomizerRegistry();
+    }
+
+    /**
+     * 骨架Handler的核心Aspect, 必须在WebsocketPlusBeanFactoryPostProcessor前注入到容器.
+     * 否则在#createBean时, 无法对注入的bean对象cglib代理.
      */
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
@@ -47,12 +70,12 @@ public class WebsocketPlusAutoConfiguration {
     }
 
     @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer(@Autowired WebsocketPlusProperties websocketPlusProperties) {
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
         ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setAsyncSendTimeout(websocketPlusProperties.getAsyncSendTimeout());
-        container.setMaxSessionIdleTimeout(websocketPlusProperties.getMaxSessionIdleTimeout());
-        container.setMaxBinaryMessageBufferSize(websocketPlusProperties.getMaxBinaryMessageBufferSize());
-        container.setMaxTextMessageBufferSize(websocketPlusProperties.getMaxTextMessageBufferSize());
+        container.setAsyncSendTimeout(WebsocketPlusProperties.asyncSendTimeout);
+        container.setMaxSessionIdleTimeout(WebsocketPlusProperties.maxSessionIdleTimeout);
+        container.setMaxBinaryMessageBufferSize(WebsocketPlusProperties.maxBinaryMessageBufferSize);
+        container.setMaxTextMessageBufferSize(WebsocketPlusProperties.maxTextMessageBufferSize);
         return container;
     }
 
@@ -62,12 +85,18 @@ public class WebsocketPlusAutoConfiguration {
         return new WebsocketProcessorAttributes();
     }
 
+    /**
+     * 扫描骨架handler必要参数.
+     */
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public WebsocketPlusHandlerPostProcessor websocketPlusHandlerPostProcessor() {
         return new WebsocketPlusHandlerPostProcessor();
     }
 
+    /**
+     * 扫描全局异常处理必要参数.
+     */
     @Bean
     @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public WebsocketPlusExceptionPostProcessor websocketPlusExceptionPostProcessor() {
