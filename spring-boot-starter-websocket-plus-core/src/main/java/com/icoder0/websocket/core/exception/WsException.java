51a05aaf8fbbbd7fd2334f7a5a5c5ef155db979d
bofa1ex <bofa1exx@gmail.com>
2020-09-07 01:58:37 +0800

    - chore: 移除冗余构造方法.

--- spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsException.java
+++ spring-boot-starter-websocket-plus-core/src/main/java/com/icoder0/websocket/core/exception/WsException.java
@@ -1,15 +1,16 @@
 package com.icoder0.websocket.core.exception;
 
+import com.icoder0.websocket.core.model.WsBusiCode;
 import lombok.Getter;
 
 /**
  * @author bofa1ex
  * @since 2020/8/22
  */
-@Getter
-public class WsException extends RuntimeException {
 
-    private WsBusiCode wsBusiCode;
+public class WsException extends RuntimeException {
+    @Getter
+    private final WsBusiCode wsBusiCode;
 
     public WsException(WsBusiCode wsBusiCode) {
         this.wsBusiCode = wsBusiCode;
@@ -17,11 +18,6 @@ public class WsException extends RuntimeException {
 
     public WsException(WsBusiCode wsBusiCode, String message) {
         super(message);
-        this.wsBusiCode = wsBusiCode;
-    }
-
-    public WsException(WsBusiCode wsBusiCode, String message, Throwable cause) {
-        super(message, cause);
         this.wsBusiCode = wsBusiCode;
     }
 }
