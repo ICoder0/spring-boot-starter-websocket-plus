b4e3515c0d73b5fe5abb30b5778049646dd49190
bofa1ex <bofa1exx@gmail.com>
2020-09-07 01:55:00 +0800

    - add: 新增入口示例.

---
+++ spring-boot-starter-websocket-plus-example/src/main/java/com/icoder0/websocket/example/WsSpecificationTest.java
@@ -0,0 +1,45 @@
+package com.icoder0.websocket.example;
+
+import com.google.common.collect.ImmutableMap;
+import com.icoder0.websocket.annotation.*;
+import com.icoder0.websocket.core.model.WsOutboundBean;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Map;
+
+/**
+ * @author bofa1ex
+ * @since 2020/9/4
+ */
+@Slf4j
+@WebsocketMapping(value = "/api/mirai", prototype = true)
+public class WsSpecificationTest {
+
+    /**
+     * 默认入参都从payload中提取, 如果需要提取header字段, 需要声明@WebsocketHeader.
+     * 如果业务参数中有字段名与header系统字段冲突, 需要声明@WebsocketPayload, 并声明该字段在payload中的字段名, 否则编译阶段不通过.
+     */
+    @WebsocketMethodMapping("#inbound.code == 8001")
+    public WsOutboundBean<?> login2(WebSocketSession session,
+                                    @WebsocketHeader(isSequence = true) Long seq,
+                                    @WebsocketPayload(value = "sequence") Long nestSeq,
+                                    @WebsocketHeader(isFunctionCode = true) Integer code,
+                                    Integer ver,
+                                    PayloadVO req,
+                                    Map<String, Object> reqDup,
+                                    @WebsocketPayload("params") Map<String, Object> innerParams
+    ) {
+        log.info("test#seq {}", seq);
+        return WsOutboundBean.ok(ImmutableMap.of(
+                "hello", "world"
+        ));
+    }
+
+    @Data
+    public static class PayloadVO {
+        private Integer ver;
+        private String params;
+    }
+}
