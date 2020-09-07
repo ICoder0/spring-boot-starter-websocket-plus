57891c407acbc1723e16b5e92bbc2466f8a73c33
bofa1ex <bofa1exx@gmail.com>
2020-09-07 17:18:03 +0800

    - chore: 更新示例.

--- spring-boot-starter-websocket-plus-example/src/main/java/com/icoder0/websocket/example/WsSpecificationTest.java
+++ spring-boot-starter-websocket-plus-example/src/main/java/com/icoder0/websocket/example/WsSpecificationTest.java
@@ -21,19 +21,19 @@ public class WsSpecificationTest {
      * 默认入参都从payload中提取, 如果需要提取header字段, 需要声明@WebsocketHeader.
      * 如果业务参数中有字段名与header系统字段冲突, 需要声明@WebsocketPayload, 并声明该字段在payload中的字段名, 否则编译阶段不通过.
      */
-    @WebsocketMethodMapping("#inbound.code == 8001")
-    public String login2(WebSocketSession session,
-                                    @WebsocketHeader(isSequence = true) Long seq,
-                                    @WebsocketPayload(value = "sequence") Long nestSeq,
-                                    @WebsocketHeader(isFunctionCode = true) Integer code,
-                                    Integer ver,
-                                    PayloadVO req,
-                                    Map<String, Object> reqDup,
-                                    @WebsocketPayload("params") Map<String, Object> innerParams
-    ) {
-        log.info("test#seq {}", seq);
-        return "123";
-    }
+//    @WebsocketMethodMapping("#inbound.code == 8001")
+//    public String login2(WebSocketSession session,
+//                                    @WebsocketHeader(isSequence = true) Long seq,
+//                                    @WebsocketPayload(value = "sequence") Long nestSeq,
+//                                    @WebsocketHeader(isFunctionCode = true) Integer code,
+//                                    Integer ver,
+//                                    PayloadVO req,
+//                                    Map<String, Object> reqDup,
+//                                    @WebsocketPayload("params") Map<String, Object> innerParams
+//    ) {
+//        log.info("test#seq {}", seq);
+//        return "123";
+//    }
 
     @Data
     public static class PayloadVO {
